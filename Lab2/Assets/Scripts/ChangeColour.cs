530835b27449ea2f91fc469482af928aa6c14cce
Adriana Cuellas <32887126+acuellas@users.noreply.github.com>
2018-02-04 14:21:35 -0500

    Final version

---
+++ Lab2/Assets/Scripts/ChangeColour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColour : MonoBehaviour {
+
+    public GameObject Adriana1;
+
+    public Sprite firstImage;
+    public Sprite secondImage;
+    public Sprite thirdImage;
+    public int buttonNumber;
+
+    private Rigidbody2D rBody;
+    private SpriteRenderer sRend;
+    
+	// Use this for initialization
+	void Start () {
+
+        rBody = Adriana1.GetComponent<Rigidbody2D>();
+        sRend = Adriana1.GetComponent<SpriteRenderer>();
+
+        
+	}
+	
+	void OnMouseDown () {
+        if (buttonNumber == 1)
+        {
+            sRend.sprite = firstImage;
+            
+        } else if (buttonNumber == 2)
+        {
+            sRend.sprite = thirdImage;
+           
+        }
+        else
+        {
+            sRend.sprite = secondImage;
+            
+        }
+		
+	}
+
+
+}
